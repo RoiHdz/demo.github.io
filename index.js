43c9ef304f1a2d405e0a9d07ac1db5746fbc1924
Rodrigo Hernandez <rodrigophernandez@hotmail.com>
2022-02-26 20:16:57 -0600

    Cambio de pagina
    Se cambio de proyecto

--- index.js
+++
@@ -1,6 +0,0 @@
-let navToggle = document.querySelector(".nav-toggle");
-let navMenu = document.querySelector(".navbar-responsive");
-
-navToggle.addEventListener("click",() => {
-	navMenu.classList.toggle("nav-active")
-})
